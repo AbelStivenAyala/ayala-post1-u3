b3b53184f4594de77d789dd667fdeac79d1471a4
Abel Stiven Ayala Llanes <abelstivenal@gmail.com>
2026-09-20 19:52:54 -0500

    test: agregar pruebas JUnit 5 de asistencia y emision de certificados (Parte 1)

--- src/main/java/com/universidad/confudes/ConfUdesApp.java
+++ src/main/java/com/universidad/confudes/ConfUdesApp.java
@@ -2,10 +2,42 @@ package com.universidad.confudes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.universidad.confudes.certificados.EnvioCorreoService;
+import com.universidad.confudes.certificados.FirmaDigitalService;
+import com.universidad.confudes.certificados.GeneradorCertificadoPDF;
+import com.universidad.confudes.certificados.ValidadorAsistencia;
+import com.universidad.confudes.externo.qrcheck.QRCheckClient;
 
 @SpringBootApplication
 public class ConfUdesApp {
     public static void main(String[] args) {
         SpringApplication.run(ConfUdesApp.class, args);
+    }
+
+    @Bean
+    public QRCheckClient qrCheckClient() {
+        return new QRCheckClient();
+    }
+
+    @Bean
+    public ValidadorAsistencia validadorAsistencia() {
+        return new ValidadorAsistencia();
+    }
+
+    @Bean
+    public GeneradorCertificadoPDF generadorCertificadoPDF() {
+        return new GeneradorCertificadoPDF();
+    }
+
+    @Bean
+    public FirmaDigitalService firmaDigitalService() {
+        return new FirmaDigitalService();
+    }
+
+    @Bean
+    public EnvioCorreoService envioCorreoService() {
+        return new EnvioCorreoService();
     }
 }
